14053bb88e0ee9657b8b4347a9c3af33d2e2ca43
anmol.monga <anmol.monga@mhealth.tech>
2025-07-26 04:05:05 +0530

    fix

--- app.py
+++ app.py
@@ -619,6 +619,6 @@ def after_request(response):
 
 
 # Production WSGI application
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))
-    app.run(host='0.0.0.0', port=port, debug=app.config['DEBUG'])
+    if __name__ == '__main__':
+        port = int(os.environ.get('PORT', 5001))
+        app.run(host='0.0.0.0', port=port, debug=False)
